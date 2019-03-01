e24e260c3b6c805e2cbf328db4e66d7b530c8127
chenweijie <705404748@qq.com>
2019-03-01 17:11:27 +0800

    RestTempalte三种方式的使用

---
+++ src/main/java/com/cwjie/order/config/RestTemplateConfig.java
@@ -0,0 +1,21 @@
+package com.cwjie.order.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @program: eureka
+ * @author: chenweijie
+ * @date: 2019/03/01
+ **/
+@Component
+public class RestTemplateConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
--- src/main/java/com/cwjie/order/controller/ClientController.java
+++ src/main/java/com/cwjie/order/controller/ClientController.java
@@ -1,7 +1,14 @@
 package com.cwjie.order.controller;
 
+import com.cwjie.order.config.RestTemplateConfig;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * @program: order
@@ -12,8 +19,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClientController {
 
+    @Resource
+    private LoadBalancerClient loadBalancerClient;
+
+    @Resource
+    private RestTemplate restTemplate;
+
     @GetMapping("/getProductMsg")
     public String getProductMsg(){
-        return null;
+        // 第一种方式
+        /*RestTemplate restTemplate = new RestTemplate();
+        String forObject = restTemplate.getForObject("http://localhost:8080/msg", String.class);*/
+
+        // 第二种方式(利用LoadBalancerClient通过应用名获取url，然后再使用restTemplate)
+        /*RestTemplate restTemplate = new RestTemplate();
+        ServiceInstance instance = loadBalancerClient.choose("PRODUCT");
+        String url = String.format("http://%s:%s",instance.getHost(),instance.getPort() + "/msg");
+        String forObject = restTemplate.getForObject(url, String.class);*/
+
+        // 第三种方式(利用@LoadBalanced,可在restTempalte里使用应用名字)
+        String forObject = restTemplate.getForObject("http://PRODUCT/msg", String.class);
+
+        return forObject;
+
     }
 }
